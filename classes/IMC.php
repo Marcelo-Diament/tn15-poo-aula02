2b6b8aa2da84800b932470474ed33a522c80f540
Marcelo Diament <marcelodiament@gmail.com>
2020-05-18 19:30:22 -0300

    criando a interface IMC

---
+++ classes/IMC.php
@@ -0,0 +1,11 @@
+<?php
+
+    // Criando uma interface chamada IMC
+    interface IMC {
+
+        // Vamos declarar um método (que não pode ser detalhado, apenas declarado)
+        public function calcularIMC();
+
+    }
+
+?>
